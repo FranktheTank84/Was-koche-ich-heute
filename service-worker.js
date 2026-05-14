78ff748adeceb8b454879190d16ceab5875d9b3d
FranktheTank84 <lwlam460@gmail.com>
2026-05-14 08:19:16 +0000

    Service Worker

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'kuechenbasis-v1';
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        '/Was-koche-ich-heute/kuechen-app.html',
+        '/Was-koche-ich-heute/manifest.json'
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.match(event.request)
+    )
+  );
+});
